5f9dc1775f4ee34dca90f5040d9cba98e149ea17
ayushmankumar7 <ayushmankumar7@gmail.com>
2020-09-10 17:35:27 +0530

    res bottleneck block

--- src/models/model_block.py
+++ src/models/model_block.py
@@ -25,3 +25,20 @@ def _res_bottleneck(inputs, filters, kernel, t, s, r = False):
 
     tchannel = tf.keras.backend.int_shape(inputs)[-1] * t
 
+    x = conv_block(inputs, "conv", tchannel, (1,1), strides =(1,1) )
+
+    x = tf.keras.layers.DepthWiseConv2D(kernel, strides = (s,s), depth_multiplier = 1, padding = 'same')(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.activations.relu(x)
+
+    x = conv_block(x, 'conv', filters, (1,1), strides =(1,1), padding = 'same', relu = False)
+
+    if r:
+        x = tf.keras.keras.layers.add([x, inputs])
+
+    return x
+
+
+
+
+
