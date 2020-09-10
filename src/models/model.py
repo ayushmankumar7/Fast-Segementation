3eba16f6bd7c48e24910708cf4cf3b718a46afae
ayushmankumar7 <ayushmankumar7@gmail.com>
2020-09-10 18:45:08 +0530

    model updated

--- src/models/model.py
+++ src/models/model.py
@@ -20,3 +20,15 @@ gfe_layer = bottleneck_block(gfe_layer, 128,(3,3), t = 6, strides = 1, n=3)
 # PPM 
 
 gfe_layer = pyramid_pooling_block(gfe_layer, [2,4,6,8])
+
+# Feature Fusion 
+
+ff_layer1 = conv_block(lds_layer, 'conv', 128, (1,1), padding='same', strides = (1,1), relu=False )
+
+ff_layer2 = tf.keras.layers.UpSampling2D((4,4))(gfe_layer)
+ff_layer2 = tf.keras.layers.DepthWiseConv2D(128, strides = (1,1), depth_multiplier=1, padding='same')(ff_layer2)
+ff_layer2 = tf.keras.layers.BatchNormalization()(ff_layer2)
+ff_layer2 = tf.keras.activations.relu(ff_layer2)
+ff_layers2 = tf.keras.layers.Conv2D(128, 1, 1, padding='same', activation = None)(ff_layer2)
+
+
