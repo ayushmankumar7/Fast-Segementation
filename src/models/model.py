5593a4a2327049633e857d47b081acde2397cc45
ayushmankumar7 <ayushmankumar7@gmail.com>
2020-09-10 17:25:00 +0530

    model updated

--- src/models/model.py
+++ src/models/model.py
@@ -1,7 +1,13 @@
 import tensorflow as tf 
-import model_block
+from model_block import * 
 
 
 input_layer = tf.keras.layers.Input(shape = (2048, 1024, 3), name = "input_layer")
+
+
+lds_layer = conv_block(input_layer, 'conv', 32,(3,3), strides = (2,2) )
+lds_layer = conv_block(lds_layer, 'ds', 48,(3,3), strides = (2,2) )
+lds_layer = conv_block(input_layer,'ds', 64,(3,3), strides = (2,2) )
+
 
 
