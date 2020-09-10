c81eb02458ed80964a3534e87c6e7dbc712b4fb2
ayushmankumar7 <ayushmankumar7@gmail.com>
2020-09-10 17:18:58 +0530

    Conv_block added

---
+++ src/models/model_block.py
@@ -0,0 +1,22 @@
+import tensorflow as tf 
+
+
+
+def conv_block(inputs, conv_typem kernel, kernel_size, strides, padding="same", relu = True):
+
+    if (conv_type == 'ds'):
+        
+        x = tf.keras.layers.SeperableConv2D(kernel, kernel_size, padding = padding, strides = strides)(inputs)
+    
+    else:
+        
+        x = tf.keras.layers.Conv2D(kernel, kernel_size, padding = padding, strides = strides)(inputs)
+
+    x = tf.keras.layers.BatchNormalization()(x)
+
+    if(relu):
+        x = tf.keras.activations.relu(x)
+
+    return x 
+
+
